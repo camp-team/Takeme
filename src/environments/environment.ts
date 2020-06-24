74ad8cdb12c4cf28ddbdb86de08e4d077a84cbbb
ason.kanamori <asung7@live.jp>
2020-06-24 20:11:43 +0900

    feat(firebase): firebaseとAngularを連携

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBeg26cXP51lg4_14fI1oyT8SqXXUxX8mI',
+    authDomain: 'takeme-77479.firebaseapp.com',
+    databaseURL: 'https://takeme-77479.firebaseio.com',
+    projectId: 'takeme-77479',
+    storageBucket: 'takeme-77479.appspot.com',
+    messagingSenderId: '442494793217',
+    appId: '1:442494793217:web:d6e8b0da5d96d480573200',
+    measurementId: 'G-WBPD5ETMXT',
+  },
 };
 
 /*
