6f056a3be6ec8f519d33f292a5485e9dea76bd70
ason.kanamori <asung7@live.jp>
2020-07-03 19:58:54 +0900

    feat(top-module): Mat-icon導入#32

--- src/app/top/top.module.ts
+++ src/app/top/top.module.ts
@@ -7,9 +7,16 @@ import { MainVisualComponent } from '../main-visual/main-visual.component';
 import { NewArrivalComponent } from '../new-arrival/new-arrival.component';
 import { MatCardModule } from '@angular/material/card';
 import { SwiperModule } from 'ngx-swiper-wrapper';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [TopComponent, MainVisualComponent, NewArrivalComponent],
-  imports: [CommonModule, TopRoutingModule, MatCardModule, SwiperModule],
+  imports: [
+    CommonModule,
+    TopRoutingModule,
+    MatCardModule,
+    SwiperModule,
+    MatIconModule,
+  ],
 })
 export class TopModule {}
