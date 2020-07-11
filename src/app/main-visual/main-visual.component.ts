e0e899a73b3c7d869d90485fb462aac702a42a1d
ason.kanamori <asung7@live.jp>
2020-07-11 14:28:51 +0900

    feat(main-component): スライドショー実装

--- src/app/main-visual/main-visual.component.ts
+++ src/app/main-visual/main-visual.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-visual',
   templateUrl: './main-visual.component.html',
-  styleUrls: ['./main-visual.component.scss']
+  styleUrls: ['./main-visual.component.scss'],
 })
 export class MainVisualComponent implements OnInit {
+  constructor() {
+    const picsSrc = [
+      '/assets/images/res-1.jpeg',
+      '/assets/images/res-2.jpeg',
+      '/assets/images/res-3.jpeg',
+    ];
+    let num = -1;
 
-  constructor() { }
+    function slideshow_timer() {
+      if (num === 2) {
+        num = 0;
+      } else {
+        num++;
+      }
+      document.getElementById('mypic').src = picsSrc[num];
+    }
 
-  ngOnInit(): void {
+    setInterval(slideshow_timer, 3000);
   }
-
+  ngOnInit(): void {}
 }
