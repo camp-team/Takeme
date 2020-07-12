d937951ab9c2a012512f4d9f6a438dc8a4d25d3a
ason.kanamori <asung7@live.jp>
2020-07-12 09:24:18 +0900

    feat(auth.service):　ログイン昨日実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Takeme';
+  user$ = this.authService.user$;
+
+  constructor(private authService: AuthService) {}
+
+  login() {
+    this.authService.login();
+  }
+  logout() {
+    this.authService.logout();
+  }
 }
---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,6 @@
+export interface User {
+  name: string;
+  avatarURL: string;
+  createdAt: Date;
+  email: string;
+}
