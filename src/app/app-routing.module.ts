92b9a7b2390f597c3ae07f69e5efc286ea51cc98
ason.kanamori <asung7@live.jp>
2020-07-15 21:47:26 +0900

    feat(auth.guard): 認証ガード

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TopModule } from './top/top.module';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     loadChildren: () => import('./top/top.module').then((m) => m.TopModule),
+    canLoad: [AuthGuard],
+    canActivate: [AuthGuard],
   },
 ];
 
