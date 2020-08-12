afec99a94d550eb160a461b98537756319ee2066
ason.kanamori <asung7@live.jp>
2020-08-12 09:11:49 +0900

    feat(mypage): マイページ実装　#50

--- src/app/mypage/mypage.module.ts
+++ src/app/mypage/mypage.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { MypageRoutingModule } from './mypage-routing.module';
 import { MypageComponent } from './mypage/mypage.component';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [MypageComponent],
   imports: [
     CommonModule,
-    MypageRoutingModule
-  ]
+    MypageRoutingModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+  ],
 })
-export class MypageModule { }
+export class MypageModule {}
