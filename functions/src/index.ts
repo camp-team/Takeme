3e48460078e40681c2b12328fdec8449ca494f25
Kim A sung <kimasung@Kim-no-MacBook-Pro.local>
2020-10-03 13:05:11 +0900

    feat(index): region変更

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,17 +2,26 @@ import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
 admin.initializeApp();
+
+export { tweet } from './tweet.function';
+
 const db = admin.firestore();
 
-export const createUser = functions.auth.user().onCreate((user) => {
-  return db.doc(`users/${user.uid}`).set({
-    name: user.displayName,
-    avatarURL: user.photoURL,
-    email: user.email,
-    createdAt: new Date(),
+export const createUser = functions
+  .region('asia-northeast1')
+  .auth.user()
+  .onCreate((user) => {
+    return db.doc(`users/${user.uid}`).set({
+      name: user.displayName,
+      avatarURL: user.photoURL,
+      email: user.email,
+      createdAt: new Date(),
+    });
   });
-});
 
-export const deleteUser = functions.auth.user().onDelete((user) => {
-  return db.doc(`users/${user.uid}`).delete();
-});
+export const deleteUser = functions
+  .region('asia-northeast1')
+  .auth.user()
+  .onDelete((user) => {
+    return db.doc(`users/${user.uid}`).delete();
+  });
